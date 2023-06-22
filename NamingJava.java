7a58ad6ac7fc8e6f43fe1298092bd7ec13ac34f7
Diglas Temoche <104596485+DiTem1010@users.noreply.github.com>
2023-06-22 17:44:56 -0500

    Create NamingJava.java

---
+++ NamingJava.java
@@ -0,0 +1,24 @@
+//Upper Camel Case in the class
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 3333777;
+        System.out.println(celphone);
+
+        int celPhone = 1234567;
+        System.out.println(celPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency$ = "PEN";
+        String background_color = "RED";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+        //Lower Camel Case
+        String fullName = "Diglas Hendrik Temoche Rodriguez";
+        int sizeInCentimeters = 26;
+    }
+}
